0d7c144f68a8bea32bc7ad34036be38f4d5732dd
Federico Arredondo <federicoarredondoc@gmail.com>
2022-03-21 10:09:09 -0300

    feat: basic /ping command, changed structure a bit

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const dotenv = require('dotenv');
 
+const discordJs = require('./discord_js_setup');
+
+// .env config
 dotenv.config();
 
-client.on('ready', () => {
-	console.log('Hey!');
-});
-
-client.login(process.env.DISCORD_TOKEN);
+discordJs.setup(process.env.DISCORD_TOKEN);
