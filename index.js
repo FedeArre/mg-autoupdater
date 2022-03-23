a401abbd5b69571bf1a0f7099edd2f71102a2de6
Federico Arredondo <federicoarredondoc@gmail.com>
2022-03-23 08:12:56 -0300

    basic Windows Forms app project (fixed)

--- index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-const discordJs = require('./discord_js_setup');
-const express_handler = require('./backend/express_handler');
-
-// .env config
-dotenv.config();
-
-discordJs.setup(process.env.DISCORD_TOKEN);
